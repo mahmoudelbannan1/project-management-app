d6805a636152b5544152e4e7a03b9a4a4c20ecda
mahmoud khaled <mahmoudelbannan8@gmail.com>
2024-04-30 02:11:58 +0300

    first commit

---
+++ src/components/ProjectsSidebare.jsx
@@ -0,0 +1,15 @@
+const ProjectsSidebar = () => {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul>
+        <li></li>
+      </ul>
+    </aside>
+  );
+};
+
+export default ProjectsSidebar;
